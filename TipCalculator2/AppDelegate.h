2e0c759312235c01abce6a354dd5ec87479b82d3
Autumn Victor <autumnv@stationline-lm.corp.yahoo.com>
2014-01-09 16:13:00 -0800

    Initial Commit

---
+++ TipCalculator2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TipCalculator2
+//
+//  Created by Autumn Victor on 1/9/14.
+//  Copyright (c) 2014 Autumn Victor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
